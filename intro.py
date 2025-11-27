b26f086bcbc6b4caa175a51c6893fd1b099f62bf
fjavier <fjavier.mg28@gmail.com>
2025-11-26 19:15:28 -0600

    actualizado ejercicios de tipos de datos y formato de strings

---
+++ intro.py
@@ -0,0 +1,3 @@
+""" ESTE ESTRING ARREGLA EL SGUNDO ERROR"""
+print("Hola Mundo!")
+print("el weta " * 4)
---
+++ tipos/04-metodos-strings.py
@@ -0,0 +1,6 @@
+animal = " chanCHito  feliz "
+print(animal.upper())
+print(animal.lower())
+print(animal.capitalize())
+print(animal.title())
+print(animal.strip())
